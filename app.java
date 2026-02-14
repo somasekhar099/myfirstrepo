f0e54dc469ac7eb8d33139d3dd385aaf0b3654f2
somasekhar099 <sekharchowdary000@gmail.com>
2026-02-14 19:26:16 +0530

    Add multiplication by 3 for product output
    adding one line of code

--- app.java
+++ app.java
@@ -8,5 +8,7 @@ public class MultiplyTwoNumbers {
         float product = first * second;
 
         System.out.println("The product is: " + product);
+        System.out.println("The product is: " + product * 3);
+        
     }
 }
